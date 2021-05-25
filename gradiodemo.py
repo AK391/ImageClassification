5fa1c3dc5424342fa70ddba1d32affc941e1b5c8
AK391 <ahsengradio@gmail.com>
2021-05-25 12:57:15 -0400

    remove

--- gradiodemo.py
+++ gradiodemo.py
@@ -12,10 +12,6 @@ io2 = gr.Interface.load("huggingface/facebook/deit-base-distilled-patch16-224")
 title = "VIT and Deit Parallel Demo"
 description = "demo for Google VIT and Facebook Deit using Gradio parallel and Huggingface Models. Upload an image or click an example image to use. Read more at the links below"
 article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2010.11929'>VIT</a> | <a href='https://github.com/google-research/vision_transformer'>Vision Transformer Github</a> | <a href='https://arxiv.org/abs/2012.12877'>Deit</a>| <a href='https://github.com/facebookresearch/deit'>Deit Github</a> | <a href='https://gradio.app/blog/using-huggingface-models'>Gradio Blog</a></p>"
-inputs = gr.inputs.Image(label="Input Image")
-outputs = [
-    gr.outputs.Label(label="VIT"), 
-    gr.outputs.Label(label="Deit")
-]
+
 examples = [['cat.jpg'], ['dog.jpg']]
-Parallel(io1, io2, inputs, outputs, title=title, description=description, article=article, examples=examples).launch()
+Parallel(io1, io2, title=title, description=description, article=article, examples=examples).launch()
